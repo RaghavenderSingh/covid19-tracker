06412820df7cb986e0de8c2a018b25b1ff29eb96
RaghavenderSingh <92844911+RaghavenderSingh@users.noreply.github.com>
2022-02-04 02:31:47 +0530

    Updates

---
+++ precache-manifest.01d4fb50f7af9720ce83337f23c5cd56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76cf26028b99d7c3e6ab9aa5b81e379",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "3f2e357cc3b49839781d",
+    "url": "/react-gh-pages/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "e58a3a4844d477548448",
+    "url": "/react-gh-pages/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "3f2e357cc3b49839781d",
+    "url": "/react-gh-pages/static/js/2.4d432e43.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/react-gh-pages/static/js/2.4d432e43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e58a3a4844d477548448",
+    "url": "/react-gh-pages/static/js/main.a65219a2.chunk.js"
+  },
+  {
+    "revision": "5dcafb6368b7de298328",
+    "url": "/react-gh-pages/static/js/runtime-main.a5db3c6c.js"
+  }
+]);
